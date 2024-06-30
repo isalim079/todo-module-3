470d21e8354ff44777897edbcad721d2b6db1e3f
isalim079 <salimreza1025@gmail.com>
2024-06-30 12:56:37 +0600

    [redux] features folder created. hook and store file created. useAppDispatch and selector copied from redux toolkit typescript documentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
     return (
-        <>
+        <div className="">
             <h1>Todo App</h1>
-        </>
+            <Button>Go</Button>
+        </div>
     );
 }
 
